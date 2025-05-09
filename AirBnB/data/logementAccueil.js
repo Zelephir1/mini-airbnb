1fa7f3460a98e7d3622da4015f43ef5ceb6dfd84
Hugo Cabanes <hugo33tab@gmail.com>
2025-05-09 17:56:30 +0200

    BeforeDynamicAccueil

---
+++ AirBnB/data/logementAccueil.js
@@ -0,0 +1,137 @@
+const logementAccueil = [
+    {
+      id: 1,
+      titre: "Forteresse Noir",
+      image: "/images/1.jpg",
+      ville: "Paris",
+      type: "studio",
+      prix: 536,
+      note: 4.3,
+      hote: "Particulier exerçant l'activité d'hôte",
+      disponibilite: "24-29 Juin"
+    },
+    {
+      id: 2,
+      titre: "Anor Londo",
+      image: "/images/2.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 1340,
+      note: 3.6,
+      hote: "Particulier exerçant l'activité d'hôte",
+      disponibilite: "19-24 Juin"
+    },
+    {
+      id: 3,
+      titre: "Irithyll de la Valée Boréale",
+      image: "/images/3.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 2750,
+      note: 4.2,
+      hote: "Particulier exerçant l'activité d'hôte",
+      disponibilite: "15-20 Juin"
+    },
+    {
+      id: 4,
+      titre: "Forteresse de l'Empereur des Hommes",
+      image: "/images/4.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 18700,
+      note: 2.6,
+      hote: "Particulier exerçant l'activité d'hôte",
+      disponibilite: "18-23 Juin"
+    },
+    {
+      id: 5,
+      titre: "Laputa",
+      image: "/images/5.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 3250,
+      note: 4.9,
+      hote: "Professionnel",
+      disponibilite: "22-27 Juin"
+    },
+    {
+      id: 6,
+      titre: "Le Château Ambulant",
+      image: "/images/6.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 235,
+      note: 4.6,
+      hote: "Particulier exerçant l'activité d'hôte",
+      disponibilite: "16-21 Juin"
+    },
+    {
+      id: 7,
+      titre: "L'hôtel de Chihiro",
+      image: "/images/7.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 73,
+      note: 4.4,
+      hote: "Professionnel",
+      disponibilite: "23-27 Juin"
+    },
+    {
+      id: 8,
+      titre: "Maison Totoro",
+      image: "/images/8.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 145,
+      note: 5.0,
+      hote: "Particulier exerçant l'activité d'hôte",
+      disponibilite: "19-24 Juin"
+    },
+    {
+      id: 9,
+      titre: "Maison de Gru",
+      image: "/images/9.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 92,
+      note: 1.8,
+      hote: "Particulier exerçant l'activité d'hôte",
+      disponibilite: "21-26 Juin"
+    },
+    {
+      id: 10,
+      titre: "Palais du Héron Blanc",
+      image: "/images/10.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 1322,
+      note: 4.7,
+      hote: "Professionnel",
+      disponibilite: "18-23 Juin"
+    },
+    {
+      id: 11,
+      titre: "Chapelle d'Ariandel",
+      image: "/images/11.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 68,
+      note: 3.2,
+      hote: "Particulier exerçant l'activité d'hôte",
+      disponibilite: "19-24 Juin"
+    },
+    {
+      id: 12,
+      titre: "La Ville Encerclée",
+      image: "/images/12.jpg",
+      ville: "Marseille",
+      type: "villa",
+      prix: 120,
+      note: 1.5,
+      hote: "Particulier exerçant l'activité d'hôte",
+      disponibilite: "16-21 Juin"
+    },
+  ];
+  
+  module.exports = logementAccueil;
+  
--- AirBnB/routes/index.js
+++ AirBnB/routes/index.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var logementAccueil = require('../data/logementAccueil');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', logementAccueil });
 });
+
+const logements = [
+  { id: 1, titre: 'Studio à Paris', description: 'Joli studio dans le Marais' },
+  { id: 2, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+  { id: 3, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+  { id: 4, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+  { id: 5, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+  { id: 6, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+  { id: 7, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+  { id: 8, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+  { id: 9, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+  { id: 10, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+  { id: 11, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+  { id: 12, titre: 'Appartement à Lyon', description: 'Vue sur Fourvière' },
+
+  
+];
+
+router.get('/logement/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const logement = logements.find(l => l.id === id);
+
+  if (!logement) {
+    return res.status(404).send('Logement non trouvé');
+  }
+
+  res.render('logement', { logement });
+});
+
 
 module.exports = router;
 
